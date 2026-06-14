1976c04c9806d7b472fc253da173986010d06efa
Adrita-Codes <adritadas@gmail.com>
2026-06-14 17:03:14 +0530

    Add files via upload

---
+++ WEB CALCULATOR FOLDER/style2.js
@@ -0,0 +1,31 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input)
+{
+    display.value += input;
+}
+
+function clearDisplay()
+{
+       display.value = "";
+}
+
+function calculate()
+{
+ try{
+     display.value = eval(display.value);
+    }
+    catch(error)
+    {
+        display.value = "ERROR ";
+    }
+ }
+ function deleteLast()
+  {
+    display.value = display.value.slice(0, -1);
+}
+
+function percentage()
+{
+     display.value = display.value / 100;
+}
